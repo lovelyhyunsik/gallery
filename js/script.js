b75c980db9765dcd95300ceb219a854ab0e8498e
lovelyhyunsik <tlrl312@naver.com>
2022-03-03 11:59:01 +0900

    이미지업로드

--- js/script.js
+++ js/script.js
@@ -11,5 +11,27 @@ $.getJSON('./data/content.json', function(data){
 
 function initGallery(data){
     $allData=data;
-    console.log($allData);
+    //console.log($allData);
+    addItem();
+    $loadMoreBtn.click(function(){
+        addItem();
+    });
 };
+function addItem(){
+    let elements=[];
+    let slicedDate;
+    slicedDate=$allData.slice($added, $added += $addItemCount);
+    $.each(slicedDate, function(idx, item){
+        let itemHTML=
+        '<li class="gallery-item">' +
+            '<a href="'+item.images.large+'">' +
+                '<figure>' +
+                    '<img src="'+item.images.thumb+'" alt="'+item.title+'">' +
+                    '<figcaption>'+item.title+'</figcaption>'+
+                '</figure>'+
+            '</a>'+
+        '</li>';
+        elements.push($(itemHTML).get(0))
+    })
+    $container.append(elements);
+}
