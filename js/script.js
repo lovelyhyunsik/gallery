a307237b7715a2b5dd2df6ec717c52098474c44a
lovelyhyunsik <tlrl312@naver.com>
2022-03-03 11:17:05 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
-const $container=$('.gallery');//이미지들을 감싸는 갤러리
-const $loadMoreBtn=$('.load-more');//더보기 버튼
-let $addItemCount=8;//클릭할때마다 나오는 갯수
-let $added=0;//처음보이는 것 리스트 항목 모드 로드하면 버튼이 사라지게 할 용도
-let $allData=[];
+const $container=$('.gallery'); //이미지들을 감싸는 겔러리
+const $loadMoreBtn=$('.load-more'); //더보기버튼
+let $addItemCount=8; //클릭할때 나올 이미지 갯수
+let $added=0;//처음보이는 것 리스트 항복 모두 로드하면 버튼이 사라지게 할 용도
+let $allData = [];
 
+// $.getJSON(파일경로, 함수);
 $.getJSON('./data/content.json', function(data){
-    initGallery(data)
+    initGallery(data);
 });
+
 function initGallery(data){
     $allData=data;
     console.log($allData);
-}
-
+};
